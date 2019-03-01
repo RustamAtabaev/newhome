f988547f4cbd785c3541660f2062baada997ea1f
rustam <r.atabaev@vitebsk.energo.net>
2019-03-01 17:00:00 +0300

    Добавлена библиотека обрезки изображений

--- newhome/main/models.py
+++ newhome/main/models.py
@@ -1,17 +1,22 @@
 from django.db import models
+from django_thumbs.fields import ImageThumbsField
 
 # Create your models here.
+
 
 class Catalog(models.Model):
     class Meta(object):
         verbose_name = u"Категории"
         verbose_name_plural = u"Категории"
         ordering = ['catalog_order']
-    catalog_title = models.CharField(max_length=200, help_text="Название категории")
+    catalog_title = models.CharField(
+        max_length=200, help_text="Название категории")
     catalog_title.verbose_name = u"Название категории"
-    catalog_icon = models.CharField(max_length=200, help_text="Класс иконки. Пример: fas fa-stream")
+    catalog_icon = models.CharField(
+        max_length=200, help_text="Класс иконки. Пример: fas fa-stream")
     catalog_icon.verbose_name = u"Иконка категории"
-    catalog_order = models.PositiveIntegerField(default=0, blank=False, null=False)
+    catalog_order = models.PositiveIntegerField(
+        default=0, blank=False, null=False)
 
     def __str__(self):
         return self.catalog_title
@@ -21,53 +26,74 @@ class CatalogItems(models.Model):
     class Meta:
         verbose_name = u"ПодКатегории"
         verbose_name_plural = u"ПодКатегории"
-    catalog_same = models.ForeignKey(Catalog, on_delete=models.SET_NULL, null=True)
-    item_title = models.CharField(max_length=200, help_text="Название подкатегории")
+    catalog_same = models.ForeignKey(
+        Catalog, on_delete=models.SET_NULL, null=True)
+    item_title = models.CharField(
+        max_length=200, help_text="Название подкатегории")
     item_title.verbose_name = u"Название подкатегории"
-    
+
     def __str__(self):
         return self.item_title
 
+
 class SliderA(models.Model):
+
+    SIZES = (
+        {'code': 'resized', 'wxh': '800x600', 'resize': 'crop'},
+    )
+
     class Meta:
         verbose_name = u"Слайдер"
         verbose_name_plural = u"Слайдер"
     slider_title_needed = models.BooleanField(u"Нужна ли подпись к слайду?")
-    slider_title = models.CharField(max_length=200, help_text="Заголовок акции")
-    slider_img_path = models.ImageField(upload_to = "main/img/slider/")
-    
+    slider_title = models.CharField(
+        max_length=200, help_text="Заголовок акции")
+    slider_img_path = ImageThumbsField(upload_to="main/img/slider/", sizes=SIZES)
+
     def __str__(self):
         return self.slider_title
+
 
 class News(models.Model):
     class Meta:
         verbose_name = u"Новостные акуции"
         verbose_name_plural = u"Новостные акции"
-    news_img = models.ImageField(upload_to = "main/img/news/", help_text="Изображения новости", default='def')
-    news_title = models.CharField(max_length=200, help_text="Заголовок новости")
+    news_img = models.ImageField(
+        upload_to="main/img/news/", help_text="Изображения новости", default='def')
+    news_title = models.CharField(
+        max_length=200, help_text="Заголовок новости")
     news_description = models.TextField(max_length=400)
     news_text = models.TextField()
-    
+
     def __str__(self):
         return self.news_title
+
 
 class Foto(models.Model):
     class Meta:
         verbose_name = u"Фотографии магазинов"
         verbose_name_plural = u"Фотографии магазинов"
-    foto_title = models.CharField(max_length=200, help_text="Альтернатива", default='def')
-    foto_img1 = models.ImageField(upload_to = "main/img/foto/", help_text="1-ая", default='def')
-    foto_img2 = models.ImageField(upload_to = "main/img/foto/", help_text="2-ая", default='def')
-    foto_img3 = models.ImageField(upload_to = "main/img/foto/", help_text="3-я", default='def')
+    foto_title = models.CharField(
+        max_length=200, help_text="Альтернатива", default='def')
+    foto_img1 = models.ImageField(
+        upload_to="main/img/foto/", help_text="1-ая", default='def')
+    foto_img2 = models.ImageField(
+        upload_to="main/img/foto/", help_text="2-ая", default='def')
+    foto_img3 = models.ImageField(
+        upload_to="main/img/foto/", help_text="3-я", default='def')
+
     def __str__(self):
         return self.foto_title
+
 
 class Recomend(models.Model):
     class Meta:
         verbose_name = u"Рекомендации"
         verbose_name_plural = u"Рекомендации"
-    recomend_title = models.CharField(max_length=200, help_text="Заголовок", default='def')
-    recomend_img = models.ImageField(upload_to = "main/img/recomend/", help_text="картинка рекомендаций", default='def')
+    recomend_title = models.CharField(
+        max_length=200, help_text="Заголовок", default='def')
+    recomend_img = models.ImageField(
+        upload_to="main/img/recomend/", help_text="картинка рекомендаций", default='def')
 
     def __str__(self):
         return self.recomend_title
--- newhome/main/templatetags/menu.py
+++ newhome/main/templatetags/menu.py
@@ -1,6 +1,7 @@
 from django import template
 
-from main.models import Catalog, CatalogItems  # Импортируем нужные нам модели.
+# Импортируем нужные нам модели.
+from main.models import Catalog, CatalogItems, SliderA
 
 # экземпляр класса, в котором все наши теги будут зарегистрированы
 register = template.Library()
@@ -11,6 +12,14 @@ register = template.Library()
 
 @register.simple_tag
 def database_filter(value):
-  res = CatalogItems.objects.filter(catalog_same__catalog_title=value.catalog_title)
+  res = CatalogItems.objects.filter(
+      catalog_same__catalog_title=value.catalog_title)
+  return res
+
+
+@register.filter
+def cut_ext(value: str):
+  cut: str = value[:-4:]
+  res: str = cut + "-resized.jpg"
   return res
 
