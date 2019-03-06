39837c00487851c096221ee0e72560af729f6acc
rustam <r.atabaev@vitebsk.energo.net>
2019-03-06 09:03:48 +0300

    Отключена подгрузка картинок через debug

--- newhome/newhome/urls.py
+++ newhome/newhome/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Панель Администрирования'
